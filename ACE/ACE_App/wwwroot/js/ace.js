b22c2c3c792ff3fee833655cc285cec970d809ca
DESKTOP-CGLOUIV\91973 <ajoyr118@gmail.com>
2024-06-26 15:51:13 +0530

    Move js code from index.cshtml to wwwroot /js folder

---
+++ ACE/ACE_App/wwwroot/js/ace.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    // Event handler for collapse elements
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).parent().find('.glyphicon-menu-down').removeClass('glyphicon-menu-down').addClass('glyphicon-menu-up');
+    }).on('hidden.bs.collapse', function () {
+        $(this).parent().find('.glyphicon-menu-up').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down');
+    });
+
+
+    $('.aceForm').on('submit', function () {
+
+        $('#submitButton').prop("disabled", true);
+        
+        /*$('#submitButton').css("cursor", "not-allowed");*/
+
+        $(this).submit();
+    });
+});
